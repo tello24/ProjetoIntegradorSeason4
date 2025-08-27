a118ac3f1f8af64a6c453b20ba7662f36a0e5fb8
Renan <renan.schiavotello05@gmail.com>
2025-08-27 12:03:52 -0300

    UPDATE: main.dart

--- poliedro_sistema/lib/main.dart
+++ poliedro_sistema/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'pages/login_page.dart'; 
+
+import 'pages/login_page.dart';
+import 'pages/register_page.dart';
+import 'pages/prof_home.dart';
+import 'pages/aluno_home.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // inicializa com o firebase CONSEGUIEIIIHIIHIHIHIIHEEHEHEHEH
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -22,51 +26,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginPage(),
+      home: const LoginPage(), // ✅ Login decide e navega para /prof ou /aluno
       routes: {
-        '/prof': (_) => const Scaffold(body: Center(child: Text('Área do Professor'))),
-         '/aluno': (_) => const Scaffold(body: Center(child: Text('Área do Aluno'))),
+        '/register': (_) => const RegisterPage(),
+        '/prof': (_) => ProfHome(),   // sem const para evitar erro se faltar construtor const
+        '/aluno': (_) => AlunoHome(),
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() => _counter++);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Botão pressionado:'),
-            Text('$_counter vezes',
-                style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
